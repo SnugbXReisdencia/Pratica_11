1cbf513e160aca3d5e50b0b06b0ccd5a3f8578cb
SnugbXResdencia <santoseverlan29@gmail.com>
2023-09-26 08:27:38 -0300

    Correção att 1

--- Exe1.cpp
+++ Exe1.cpp
@@ -1,24 +1,19 @@
 # include <iostream>
 # include <locale>
-# include "MyFunc.h"
 
 using namespace std;
 
+void maxmin(int *vetor, int n, int &maximo, int &minimo);
+
+void preencheVetor(int vetor[], int n);
+
+void imprimeVetor(int vetor[], int n);
+
+void verificaSistemaOperacional();
+
 int main(){
 
-    int sis = system("echo hello");
-    cout << sis;
-    
-    if (sis != 0)
-    {
-        locale::global(locale(""));
-        system("clear");
-    }else{
-        setlocale(LC_ALL, "Portuguese");
-        system("cls");
-    }
-
-    
+    verificaSistemaOperacional();
 
     int vetor[10];
     int maximo, minimo;
@@ -33,4 +28,52 @@ int main(){
     cout << "O menor número e: " << minimo << endl;
 
     return 0;
+}
+
+void verificaSistemaOperacional()
+{
+    // Verifica o sistema operacional para não dar erro
+    if (system("echo ''") == 0)
+    {
+        locale::global(locale(""));
+        system("clear");
+    }
+    else
+    {
+        setlocale(LC_ALL, "Portuguese");
+        system("cls");
+    }
+}
+
+void maxmin(int *vetor, int n, int &maximo, int &minimo){
+    maximo = vetor[0];
+    minimo = vetor[0];
+    for (int i = 0; i < n; i++){
+        if (vetor[i] > maximo){
+            maximo = vetor[i];
+        }
+        if (vetor[i] < minimo){
+            minimo = vetor[i];
+        }
+    }
+}
+
+void preencheVetor(int vetor[], int n){
+    for (int i = 0; i < n; i++){
+        cout << "Digite um numero: ";
+        cin >> vetor[i];
+    }
+    system("clear");
+}
+
+void imprimeVetor(int vetor[], int n){
+    for (int i = 0; i < n; i++){
+        if(i == n-1){
+            cout << vetor[i];
+        }else{
+            cout << vetor[i] << ", ";
+
+        }
+    }
+    cout << endl;
 }
