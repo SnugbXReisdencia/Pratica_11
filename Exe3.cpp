85dbbc393a7c104dcf3a84e307d4c531c925f7c9
SnugbXResdencia <santoseverlan29@gmail.com>
2023-09-26 09:05:47 -0300

    Terminando att 3

--- Exe3.cpp
+++ Exe3.cpp
@@ -1,11 +1,55 @@
 # include <iostream>
+#include <cstring>
 
 using namespace std;
 
-// Faça uma função que receba (por referência) 4 variáveis float e ordena (crescente) os
+// Faça uma função que receba (por referência) 4 variáveis int e ordena (crescente) os
 // valores destas variáveis. Depois faça o programa principal para testar a sua função.
-void ordena(float a, float b, float c, float d){
-    float aux;
+
+void ordena(int &a, int &b, int &c, int &d);
+void limpaTela();
+
+int main(){
+    
+    limpaTela();
+
+    int a, b, c, d;
+    cout << "Digite o valor de a: ";
+    cin >> a;
+    cout << "Digite o valor de b: ";
+    cin >> b;
+    cout << "Digite o valor de c: ";
+    cin >> c;
+    cout << "Digite o valor de d: ";
+    cin >> d;
+    
+    ordena(a, b, c, d);
+    cout << a << " " << b << " " << c << " " << d;
+
+    return 0;
+}
+
+void limpaTela()
+{
+    cin.clear();
+
+    // Verifica se a variável de ambiente WINDIR está definida (ambiente Windows)
+    const char *windir = std::getenv("WINDIR");
+
+    if (windir != nullptr && std::strlen(windir) > 0)
+    {
+        // Se estiver definida, estamos no Windows, então use "cls"
+        std::system("cls");
+    }
+    else
+    {
+        // Caso contrário, estamos em um ambiente Unix/Linux, use "clear"
+        std::system("clear");
+    }
+}
+
+void ordena(int &a, int &b, int &c, int &d){
+    int aux;
     if(a > b){
         aux = a;
         a = b;
@@ -26,20 +70,14 @@ void ordena(float a, float b, float c, float d){
         b = c;
         c = aux;
     }
-}
-
-int main(){
-
-    int a, b, c, d;
-    cout << "Digite o valor de a: ";
-    cin >> a;
-    cout << "Digite o valor de b: ";
-    cin >> b;
-    cout << "Digite o valor de c: ";
-    cin >> c;
-    cout << "Digite o valor de d: ";
-    cin >> d;
-    
-
-    return 0;
+    if(b > d){
+        aux = b;
+        b = d;
+        d = aux;
+    }
+    if(c > d){
+        aux = c;
+        c = d;
+        d = aux;
+    }
 }
